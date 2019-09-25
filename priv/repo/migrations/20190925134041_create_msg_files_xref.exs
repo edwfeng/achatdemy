7b9c72c6e6163e1a24adaf965049bdc442969b2b
Edward Feng <feng.dingding@gmail.com>
2019-09-25 10:25:58 -0400

    Add seed data

--- priv/repo/migrations/20190925134041_create_msg_files_xref.exs
+++ priv/repo/migrations/20190925134041_create_msg_files_xref.exs
@@ -5,8 +5,6 @@ defmodule Achatdemy.Repo.Migrations.CreateMsgFilesXref do
     create table(:msg_files_xref) do
       add :message_id, references(:messages, on_delete: :nothing), null: false
       add :file_id, references(:files, on_delete: :nothing), null: false
-
-      timestamps()
     end
 
     create index(:msg_files_xref, [:message_id])
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,65 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Achatdemy.Repo.insert!(%Achatdemy.Users.User{
+  username: "Anthony",
+  password: "pass1",
+  email: "anthony@example.com"
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Users.User{
+  username: "Jeremy",
+  password: "pass2",
+  email: "jeremy@example.com"
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Comms.Comm{
+  name: "Default Comm"
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Users.Perm{
+  chmod: << 1 >>,
+  user_id: 1,
+  comm_id: 1
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Users.Perm{
+  chmod: << 1 >>,
+  user_id: 2,
+  comm_id: 1
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Chats.Chat{
+  title: "This is a chat",
+  type: 3,
+  author_id: 1,
+  comm_id: 1
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Chats.Message{
+  msg: "First!",
+  chat_id: 1,
+  author_id: 1
+})
+
+Achatdemy.Repo.insert!(%Achatdemy.Chats.Message{
+  msg: "Not first...",
+  chat_id: 1,
+  author_id: 2
+})
+
+msg = Achatdemy.Repo.insert!(%Achatdemy.Chats.Message{
+  msg: "Test with file",
+  chat_id: 1,
+  author_id: 1
+}) |> Achatdemy.Repo.preload(:files)
+
+file = Achatdemy.Repo.insert!(%Achatdemy.Messages.File{
+  name: "Not a virus",
+  path: "/usr/lib/virus"
+}) |> Achatdemy.Repo.preload(:messages)
+
+Ecto.Changeset.change(msg)
+|> Ecto.Changeset.put_assoc(:files, [file])
+|> Achatdemy.Repo.update!()
