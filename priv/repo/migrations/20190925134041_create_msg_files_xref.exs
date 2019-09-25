8c961ad2095d495f644c4995f3fa16004e24d69b
Edward Feng <feng.dingding@gmail.com>
2019-09-25 09:44:49 -0400

    Add xref migration

--- priv/repo/migrations/20190925134041_create_msg_files_xref.exs
+++ priv/repo/migrations/20190925134041_create_msg_files_xref.exs
@@ -3,13 +3,14 @@ defmodule Achatdemy.Repo.Migrations.CreateMsgFilesXref do
 
   def change do
     create table(:msg_files_xref) do
-      add :message_id, references(:messages, on_delete: :nothing)
-      add :file_id, references(:files, on_delete: :nothing)
+      add :message_id, references(:messages, on_delete: :nothing), null: false
+      add :file_id, references(:files, on_delete: :nothing), null: false
 
       timestamps()
     end
 
     create index(:msg_files_xref, [:message_id])
     create index(:msg_files_xref, [:file_id])
+    create index(:msg_files_xref, [:message_id, :file_id], unique: true)
   end
 end
