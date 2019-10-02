3c2f55ded03992e3b6d1fe30325347b792d9cad8
Anthony Li <li.anthony917@gmail.com>
2019-10-02 10:15:08 -0400

    Add login form

--- app/src/Login.tsx
+++ app/src/Login.tsx
@@ -1,17 +1,78 @@
 import React from 'react';
-import palette from './palette';
+import { MainPalette, DarkPalette } from './palette';
+import { ThemeProvider } from '@material-ui/styles';
+import {
+  Box,
+  Button,
+  Checkbox,
+  createMuiTheme,
+  FormControlLabel,
+  Tab,
+  Tabs,
+  TextField
+} from '@material-ui/core';
+import logo from './logo.svg';
+import {PaletteOptions} from "@material-ui/core/styles/createPalette";
 
 const backgroundStyles = {
-  backgroundColor: palette.primary.main,
-  backgroundImage: `linear-gradient(256deg, ${palette.primary.light}, ${palette.primary.dark})`,
+  backgroundColor: MainPalette.primary.main,
+  backgroundImage: `linear-gradient(256deg, ${MainPalette.primary.light}, ${MainPalette.primary.dark})`,
   width: "100%",
-  height: "100%"
+  height: "100%",
+  color: "white"
 };
 
-export default class Login extends React.Component {
+enum LoginMode {
+  LOGIN,
+  REGISTER
+}
+
+class LoginForm extends React.Component<{mode: LoginMode}, {}> {
+  render() {
+    if (this.props.mode === LoginMode.LOGIN) {
+      return (
+        <React.Fragment>
+          <Box><TextField variant="outlined" margin="dense" label="User ID (numeric uuid)" required /></Box>
+          <Box><TextField variant="outlined" margin="dense" label="Password" type="password" required /></Box>
+          <Box style={{marginTop: "1em"}}><Button variant="outlined" color="secondary">Sign in</Button></Box>
+        </React.Fragment>
+      );
+    } else if (this.props.mode === LoginMode.REGISTER) {
+      return (
+        <React.Fragment>
+          <Box><TextField variant="outlined" margin="dense" label="Username" required /></Box>
+          <Box><TextField variant="outlined" margin="dense" label="Email" required type="email" /></Box>
+          <Box><TextField variant="outlined" margin="dense" label="Password" type="password" required /></Box>
+          <Box><TextField variant="outlined" margin="dense" label="Confirm Password" type="password" required /></Box>
+          <FormControlLabel control={
+            <Checkbox checked={true} color="secondary" />
+          } label={
+            <React.Fragment>I agree to our nonexistent Terms of Service and Privacy Policy.</React.Fragment>
+          } />
+          <Box style={{marginTop: "1em"}}><Button variant="contained" color="secondary">Register</Button></Box>
+        </React.Fragment>
+      );
+    }
+
+    return null;
+  }
+}
+
+export default class Login extends React.Component<{}, {mode: LoginMode}> {
+  state = {mode: LoginMode.LOGIN};
+
   render() {
     return (
-      <div style={backgroundStyles}><p>Brian很可爱!</p></div>
+      <ThemeProvider theme={theme => createMuiTheme({...theme, palette: DarkPalette as PaletteOptions})}>
+        <div style={backgroundStyles}>
+          <img alt="Achatdemy Logo" src={logo} />
+          <Tabs value={this.state.mode} onChange={(event, newValue) => {this.setState({...this.state, mode: newValue})}}>
+            <Tab label="Login" value={LoginMode.LOGIN} />
+            <Tab label="Register" value={LoginMode.REGISTER} />
+          </Tabs>
+          <LoginForm mode={this.state.mode} />
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- app/src/palette.ts
+++ app/src/palette.ts
@@ -1,6 +1,6 @@
 import {red} from "@material-ui/core/colors";
 
-export default {
+export const MainPalette = {
   primary: {
     light: "#383e67",
     main: "#0C183C",
@@ -13,3 +13,16 @@ export default {
   },
   error: red
 };
+
+export const DarkPalette = {
+  primary: {main: "#383e67"},
+  secondary: {
+    light: "#ffe669",
+    main: "#F6B436",
+    dark: "#bf8500"
+  },
+  error: red,
+  type: "dark"
+};
+
+export default MainPalette;
