6188859e76f94404913a37db0c486b37dcc12bfe
Edward Feng <feng.dingding@gmail.com>
2019-12-02 18:48:50 -0500

    Add trigram indices

---
+++ priv/repo/migrations/20191202234720_add_trigram_indices.exs
@@ -0,0 +1,13 @@
+defmodule Achatdemy.Repo.Migrations.AddTrigramIndices do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE INDEX users_trgm_index ON users USING gin(username gin_trgm_ops)")
+    execute("CREATE INDEX comms_tgrm_index ON comms USING gin(name gin_trgm_ops);")
+  end
+
+  def down do
+    execute("DROP INDEX users_trgm_index")
+    execute("DROP INDEX comms_tgrm_index")
+  end
+end
